abffa1a78f7ba1a407db02cba646fa1ccaa7406b
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-23 20:04:03 -0400

    routing and files

--- api/routers-models/auth/auth-router.js
+++ api/routers-models/auth/auth-router.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
 
-router.get('/login', (req, res) => {
-    res.send('banana')
+// Convert role to 1 or 2 (FOREIGN KEY) or tell team to pass role in as 1 or 2
+
+router.get('/', (req, res) => {
+    res.send('auth router')
+})
+
+router.post('/register', (req, res) => {
+    res.send('register')
+})
+
+router.post('/login', (req, res) => {
+    res.send('login')
 })
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const helmet = require('helmet')
 const cors = require('cors')
 
 const authRouter = require('./routers-models/auth/auth-router')
+const classesRouter = require('./routers-models/classes/classes-router')
+const usersRouter = require('./routers-models/users/users-router')
 
 const server = express()
 server.use(express.json())
@@ -10,6 +12,8 @@ server.use(helmet())
 server.use(cors())
 
 server.use('/', authRouter)
+server.use('/users', usersRouter)
+server.use('/classes', classesRouter)
 
 server.use((err, req, res, next) => { //eslint-disable-line
     res.status(err.status || 500).json({
