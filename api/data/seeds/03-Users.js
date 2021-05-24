ac2005e5e3ddbb5f57fad7f27073cf1dbbad11a3
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-24 11:43:06 -0400

    seed data

--- api/data/seeds/03-Users.js
+++ api/data/seeds/03-Users.js
@@ -7,7 +7,6 @@ exports.seed = function(knex) {
       return knex('Users').insert([
         {username: 'admin', password: '$2b$08$EB4DmBuQGp2R6aWed7oUU.nOCTDjlAxDzAqYTbSQtrG031Dhgl8zq', email: 'admin@gmail.com', role_id: 1},
         {username: 'client', password: '$2b$08$9clNzNInduH5cbVjb3SQKOgH4A3ZrOgkilEAdYV5k3gWTC5Ka3ZG6', email: 'client@gmail.com', role_id: 2},
-        // {username: 'bababooey', password: 'password', email: 'sheesh@gmail.com', role_id: 2},
       ]);
     });
 };
