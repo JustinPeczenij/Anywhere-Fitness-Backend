8fcc53f5b6c70300f7e2501d4a1552225afbffe8
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-25 17:37:50 -0400

    wip

---
+++ api/routers/auth-router.js
@@ -0,0 +1,52 @@
+const router = require('express').Router()
+const Users = require('../routers-models/users/users-model')
+const amw = require('../routers-models/auth/auth-middleware')
+
+const bcrypt = require('bcrypt')
+const tokenBuilder = require('../routers-models/auth/tokenBuilder')
+
+/**
+ * @swagger
+ * /register:
+ *  post:
+ *    summary: register a new user to the database and return a token
+ *    responses:
+ *        201:
+ *          description: user object, token
+ *          content: application/json
+ */
+//roles are converted to 1-INSTRUCTOR or 2-CLIENT
+router.post('/register', amw.checkCredentialsRegister, amw.convertRoles, async (req, res, next) => {
+    // hash new user's password
+    try {
+        let user = req.body
+        const rounds = Number(String(process.env.BCRYPT_ROUNDS))
+        const hash = bcrypt.hashSync(user.password, rounds)
+        user.password = hash
+    // add new user to db
+        const newUser = await Users.add(req.body)
+    // create and send back a token
+        const token = tokenBuilder(newUser)
+        res.status(201).json({user: newUser, token})
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/login', amw.checkCredentialsLogin, async (req, res, next) => { 
+    try {
+        let {username, password} = req.body
+        const user = await Users.findByUsername(username)
+        if(user && bcrypt.compareSync(password, user.password)){
+            const token = tokenBuilder(user)
+            res.status(200).json({
+                message: `${user.username} was successfully logged in`,
+                token
+            });
+        } else res.status(400).json({ message: 'invalid credentials' })
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
